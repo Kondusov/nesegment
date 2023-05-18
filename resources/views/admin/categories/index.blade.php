191ae5af0b0ebc111fcf8fdfcb4e37e9d98d7ea4
Aleksandr_Kondusov <kondusov.ru@gmail.com>
2023-05-18 21:49:37 +0300

    page all category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,45 +1,70 @@
 @extends('admin.layouts.main')
 @section('content')
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-1 col-md-1 col-xs-10">
-            <a href="{{ route('admin.category.create') }}" type="button" class="btn btn-block btn-primary">Создать</a>
-          </div>
-          <div class="col-12">
-            Категории
-          </div>
-          <!-- ./col -->
-        </div>
-        <!-- /.row -->
-        <!-- Main row -->
-        <div class="row">
-            
-        </div>
-        <!-- /.row (main row) -->
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Список категорий</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard v1</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-1 col-md-1 col-xs-10">
+          <a href="{{ route('admin.category.create') }}" type="button" class="btn btn-block btn-primary">Создать</a>
+        </div>
+        <div class="col-12">
+          <div class="card-body table-responsive p-0">
+            <table class="table table-hover text-nowrap">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Название</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                  <th>Описание</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach($categories as $category)
+                <tr>
+                  <td>{{$category->id}}</td>
+                  <td>{{$category->title}}</td>
+                  <td>11-7-2023</td>
+                  <td><span class="tag tag-success">Approved</span></td>
+                  <td>Описание какое то.</td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+
+
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+      <div class="row">
+
+      </div>
+      <!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
 @endsection
